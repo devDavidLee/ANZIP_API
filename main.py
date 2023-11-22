fb90424ce7a1a6498abb2085c55608dc20533669
Damin Lee <ldm0830@gmail.com>
2023-11-23 08:43:40 +0900

    restful하게 수정

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from typing import List, Tuple
 from functions import *
 
 app = FastAPI()
-
-
-class RequestBody(BaseModel):
-    month: int
-    day: str
-    time: str
-    subwayStop: str
-    direction: str
-
 
 class ResponseBody(BaseModel):
     status: str
@@ -21,16 +12,20 @@ class ResponseBody(BaseModel):
 
 @app.get("/")
 def printinfo():
-	return "ANZIP API"
+    return "ANZIP API"
 
 @app.get("/send")
-async def your_api_function(request_body: RequestBody):
-    value = maindef(month=request_body.month, day=request_body.day, time=request_body.time,
-                    subwayStop=request_body.subwayStop, direction=request_body.direction)
+async def your_api_function(month: int, day: str, time: str, subwayStop: str, direction: str):
+    value = maindef(
+        month=month,
+        day=day,
+        time=time,
+        subwayStop=subwayStop,
+        direction=direction
+    )
     status = value[0]
     data = value[1]
 
-    # ResponseBody 모델을 이용하여 응답 데이터를 반환
     response_model = ResponseBody(status=status, data=data)
 
     return response_model
